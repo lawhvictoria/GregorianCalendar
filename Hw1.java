6e7b27316ac04c0bf29639ed8333ee9ee36aa0aa
Victoria Law <vhlaw@calpoly.edu>
2016-08-17 10:43:26 -0700

    Update Hw1.java

--- Hw1.java
+++ Hw1.java
@@ -8,7 +8,6 @@ public class Hw1 {
     //GregorianCalendar cal = new GregorianCalendar(); // Today’s date
     Calendar cal = new GregorianCalendar();
     String[] strDays = new String[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-    //String[] strDays = new String[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     //GregorianCalendar eBday = new GregorianCalendar(1919, Calendar.APRIL, 9);
     Calendar eBday = new GregorianCalendar(2015, Calendar.OCTOBER, 30);
     //Use the values Calendar.JANUARY . . . Calendar.DECEMBER to specify the month.
@@ -22,13 +21,13 @@ public class Hw1 {
     int year = cal.get(Calendar.YEAR);
     int weekday = cal.get(Calendar.DAY_OF_WEEK);
     // 1 is Sunday, 2 is Monday, . . . , 7 is Saturday
-/*    System.out.println("Calendar's Year: " + cal.get(Calendar.YEAR));
+    System.out.println("Calendar's Year: " + cal.get(Calendar.YEAR));
     System.out.println("Calendar's Day: " + cal.get(Calendar.DATE));
     System.out.println("calendar's day_of_week:" + cal.get(Calendar.DAY_OF_WEEK));
     System.out.println("calendar month:" + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH ) );
     System.out.println("bday month:" + eBday.getDisplayName(GregorianCalendar.MONTH, Calendar.LONG, Locale.ENGLISH ) );
     System.out.println("eBday Calendar's Day: " + eBday.get(Calendar.DATE));
-    System.out.println("Calendar week day:" + strDays[cal.get(Calendar.DAY_OF_WEEK) - 1]); */
+    System.out.println("Calendar week day:" + strDays[cal.get(Calendar.DAY_OF_WEEK) - 1]); 
     
     System.out.println("The date and weekday that is 100 days from today: " + strDays[cal.get(Calendar.DAY_OF_WEEK) - 1] + ", " + cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH) + " " + cal.get(Calendar.DATE) + ", " + cal.get(Calendar.YEAR));
     System.out.println("The weekday of my birthday: " + strDays[eBday.get(Calendar.DAY_OF_WEEK) - 1]);
